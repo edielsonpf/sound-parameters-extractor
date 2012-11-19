8cb4d041785f338647fce5ce345a859e2110b5f0
edielsonpf <edielsonpf@d905b40f-2bab-a8ee-70cb-6b18e34b3427>
2012-11-19 23:35:08 +0000

    New mel parameters extractor

--- workspace/source/diretivas.h
+++ workspace/source/diretivas.h
@@ -1,3 +1,7 @@
-#define	DATA_WINDOW_SIZE 		20			//20ms
-#define	DATA_WINDOW_SCALE		0.001		//ms
-#define	NUMBER_ENERGY_CONTOUR	9
+#define	DATA_WINDOW_SIZE 			20			//20ms
+#define	DATA_WINDOW_SCALE			0.001		//ms
+#define	NUMBER_ENERGY_CONTOUR		9
+
+#define	NUMBER_COEFICIENTES_MEL		12
+
+#define	USE_MEL
